7f058fe2db0f41db39ecea8f70ac547a723c1b09
firebadnofire <william@archuser.org>
2026-07-09 21:08:13 -0400

    commit

--- app/src/main/java/org/archuser/CalCount/MainActivity.kt
+++ app/src/main/java/org/archuser/CalCount/MainActivity.kt
@@ -2,6 +2,11 @@ package org.archuser.CalCount
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.graphics.Insets
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.navigateUp
@@ -34,6 +39,8 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        applySystemBarInsets()
 
         setSupportActionBar(binding.topAppBar)
 
@@ -81,5 +88,48 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun applySystemBarInsets() {
+        val toolbarStartPadding = binding.topAppBar.paddingStart
+        val toolbarTopPadding = binding.topAppBar.paddingTop
+        val toolbarEndPadding = binding.topAppBar.paddingEnd
+        val toolbarBottomPadding = binding.topAppBar.paddingBottom
+        val bottomNavStartPadding = binding.bottomNavigation.paddingStart
+        val bottomNavTopPadding = binding.bottomNavigation.paddingTop
+        val bottomNavEndPadding = binding.bottomNavigation.paddingEnd
+        val bottomNavBottomPadding = binding.bottomNavigation.paddingBottom
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { _, windowInsets ->
+            val systemBars = windowInsets.getInsets(
+                WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout()
+            )
+
+            binding.topAppBar.updatePadding(
+                left = toolbarStartPadding + systemBars.left,
+                top = toolbarTopPadding + systemBars.top,
+                right = toolbarEndPadding + systemBars.right,
+                bottom = toolbarBottomPadding
+            )
+            binding.bottomNavigation.updatePadding(
+                left = bottomNavStartPadding + systemBars.left,
+                top = bottomNavTopPadding,
+                right = bottomNavEndPadding + systemBars.right,
+                bottom = bottomNavBottomPadding + systemBars.bottom
+            )
+
+            WindowInsetsCompat.Builder(windowInsets)
+                .setInsets(
+                    WindowInsetsCompat.Type.systemBars(),
+                    Insets.of(0, 0, 0, 0)
+                )
+                .setInsets(
+                    WindowInsetsCompat.Type.displayCutout(),
+                    Insets.of(0, 0, 0, 0)
+                )
+                .build()
+        }
+
+        ViewCompat.requestApplyInsets(binding.root)
     }
 }
